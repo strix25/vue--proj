e19676d4ac7b8b3234e5c47d7d94bf119eae7f99
strix25 <damjan.oslaj@gmail.com>
2018-01-17 20:31:37 +0100

    .
    .

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -1,6 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="navbar has-shadow">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">MyCompany</a>
+        </div>
+
+        <span class="navbar-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="navbar-end navbar-menu">
+          <router-link to ="/" class="navbar-item r-item">Home</router-link>
+          <router-link to ="faq" class="navbar-item r-item">Features</router-link>
+          <router-link to ="faq" class="navbar-item r-item">About</router-link>
+          <router-link to ="faq" class="navbar-item r-item">FAQ</router-link>
+          
+          <div class="navbar-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -13,6 +44,7 @@ export default {
 
 <style lang="sass">
 @import '../node_modules/bulma/bulma.sass'
+@import 'mq'
 
 #app 
   font-family: 'Avenir', Helvetica, Arial, sans-serif
